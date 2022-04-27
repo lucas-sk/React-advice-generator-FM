faf2c680f7060e9a96e8f865dee6b175982e209d
lusk <lucas78654321@outlook.com>
2022-04-27 08:46:26 -0300

    adicionando todos os arquivos]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React, { useState } from 'react';
+import dividerMobile from '../src/assets/pattern-divider-mobile.svg'
+import dividerDesktop from '../src/assets/pattern-divider-desktop.svg'
+import dice from '../src/assets/icon-dice.svg'
+import api from './services/api'
 
 function App() {
+  const [advice, setAdvice] = useState("Click the dice to  generate one advice");
+  const [id, setId] = useState(0);
+  const handleClick = async ()=>{
+    const slip =  await api();
+    const {advice, id} = slip
+    setAdvice(advice)
+    setId(id)
+  }
+
   return (
     <div className="App">
-      
+      <div className="container">
+        <p>ADVICE #{id}</p>
+        <q>{advice}</q>
+        <picture>
+          <source srcset={dividerDesktop} media="(min-width: 1120px)"/>
+          <img src={dividerMobile} alt="divider" />
+        </picture>        
+        <button className="btn" aria-label="generate advice" onClick={() => handleClick()}>
+          <img src={dice} alt="" />
+        </button>
+      </div> 
     </div>
   );
 }
